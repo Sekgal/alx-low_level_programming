b50e603836697c3a01351c2122c132d5cfc984b2
Sekgal <kamogelosekgala321@gmail.com>
2023-06-20 06:32:29 -0700

    int main(void) { _putchar(_); _putchar(p); _putchar(u); _putchar(t); _putchar(c); _putchar(h); _putchar(a); _putchar(r); _putchar(n); return (0); clear clear clear clear

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,6 +3,7 @@
 * main - print _putchar
 * Return: 0
 */
+
 int main(void)
 {
 _putchar('_');
@@ -14,5 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0);}
+return (0);
 }
