598c9da01a28ef35db01cf80778bc7d060de8cc1
Sekgal <kamogelosekgala321@gmail.com>
2023-07-05 18:36:13 -0700

    wild

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-int actual_sqrt_recursion(int n, int i)
+int actual_sqrt_recursion(int n, int i);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root of
@@ -9,7 +10,7 @@ int actual_sqrt_recursion(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+        if (n < 0)
                 return (-1);
         return (actual_sqrt_recursion(n, 0));
 }
@@ -30,3 +31,4 @@ int actual_sqrt_recursion(int n, int i)
                 return (i);
         return (actual_sqrt_recursion(n, i + 1));
 }
+
