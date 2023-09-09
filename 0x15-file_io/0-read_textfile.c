70140bc4c478dc29f14d3e0798a7cb7e12fe52a2
Sekgal <kamogelosekgala@gmail.com>
2023-09-08 18:14:31 -0700

    user

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
-	ssize_t k;
+	ssize_t w;
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
@@ -20,10 +20,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
-	k = write(STDOUT_FILENO, buf, t);
+	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
 	close(fd);
 	return (w);
 }
-
